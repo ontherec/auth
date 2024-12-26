f85d88e1c112cd50cbfc55301e0f11d42cd90bcf
fine-pine <lsm3645@g.skku.edu>
2024-12-26 19:40:26 +0900

    feat: init project

---
+++ src/test/java/kr/ontherec/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ontherec.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
