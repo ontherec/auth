1da87082fda9840886862fcb4da51db948eaaba0
fine-pine <lsm3645@g.skku.edu>
2025-01-02 03:09:15 +0900

    chore: remove unused annotation

--- src/main/java/kr/ontherec/authorization/entity/Authority.java
+++ src/main/java/kr/ontherec/authorization/entity/Authority.java
@@ -8,10 +8,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter
 @Table(name="authorities")
 public class Authority {
 
