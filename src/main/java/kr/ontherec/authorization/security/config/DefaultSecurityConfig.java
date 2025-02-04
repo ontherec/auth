5ad68ee425a0acb2bacd5557f6a7440cd42344e1
fine-pine <lsm3645@g.skku.edu>
2025-02-04 18:19:22 +0900

    feat: Cors 설정

--- src/main/java/kr/ontherec/authorization/security/config/DefaultSecurityConfig.java
+++ src/main/java/kr/ontherec/authorization/security/config/DefaultSecurityConfig.java
@@ -1,17 +1,19 @@
 package kr.ontherec.authorization.security.config;
 
+import java.util.Collections;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration(proxyBeanMethods = false)
 public class DefaultSecurityConfig {
@@ -20,7 +22,16 @@ public class DefaultSecurityConfig {
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, AuthenticationSuccessHandler authenticationSuccessHandler) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable)
-                .headers(hc -> hc.frameOptions(FrameOptionsConfig::sameOrigin)) // h2 console
+                .cors(corsConfig -> corsConfig.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of("http://localhost:63342", "http://localhost:3000"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setExposedHeaders(Collections.singletonList("Authorization"));
+                    config.setAllowCredentials(true);
+                    config.setMaxAge(3600L);
+                    return config;
+                }))
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/.well-known/openapi-specification").permitAll()
                         .requestMatchers("/v1/signup").permitAll()
