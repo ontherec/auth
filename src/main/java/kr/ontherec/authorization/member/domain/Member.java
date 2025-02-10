5a3782d5c300ca5668cc3a11348d1c3fc995308c
fine-pine <lsm3645@g.skku.edu>
2025-02-10 17:43:23 +0900

    refactor: import -> import static

--- src/main/java/kr/ontherec/authorization/member/domain/Member.java
+++ src/main/java/kr/ontherec/authorization/member/domain/Member.java
@@ -1,11 +1,12 @@
 package kr.ontherec.authorization.member.domain;
 
-import jakarta.persistence.CascadeType;
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.FetchType.EAGER;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.Set;
@@ -24,13 +25,13 @@ import lombok.Setter;
 @Setter
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     /**
      * login id or open id
      */
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, updatable = false, nullable = false)
     private String username;
 
     private String password;
@@ -45,6 +46,6 @@ public class Member {
 
     private String picture;
 
-    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "member", fetch = EAGER, cascade = ALL, orphanRemoval = true)
     private Set<Role> roles;
 }
